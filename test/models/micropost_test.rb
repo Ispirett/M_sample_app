99b0b0469a05b4d897e209c958aaba6c46bee6e6
ispirett <ispirett@gmail.com>
2019-07-09 20:24:18 -0400

    feature(microposts): started to implement mircoposts

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+ def setup
+   @user = users(:Ispire)
+   @micropost =  @user.microposts.build(content: 'Sweet')
+ end
+
+  test 'New Post should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id should be present'do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'Content should be at most 140 characters' do
+    @micropost.content =  "a" * 200
+    assert_not @micropost.valid?
+  end
+
+  test 'Content should be the latest' do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,14 @@ class UserTest < ActiveSupport::TestCase
     test "authenticate? should return false for a user with nil digest" do
       assert_not @user.authenticated?(:remember,'')
     end
+
+    #Microposts
+    test "associated microposts should be destroyed" do
+      @user.save
+      @user.microposts.create!(content: 'New Content')
+      assert_difference 'Micropost.count', -1 do
+        @user.destroy
+      end
+
+    end
 end
